a7d120ee092a57bb3e022c7c6af67e8eabd33d08
z3t4z00k <z3t4z00k@gmail.com>
2020-09-29 19:24:33 +0530

    Added get request

--- RaspberryPi/one.py
+++ RaspberryPi/one.py
@@ -1,13 +1,26 @@
 import serial
 import time
+import requests
+import json
 
-ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
+URL = "http://54.237.104.99:443/bulbstate"
+PARAMS = {}
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 time.sleep(2)
 
-for i in range(10):
-    ser.writelines(b'H')
-    time.sleep(1)
-    ser.writelines(b'L')
-    time.sleep(1)
+while (True):
+	r = requests.get(url= URL, params = PARAMS)
+	data = r.json()
+
+	data = json.dumps(data)
+	data = data[-2]
+	print (data)
+	if(data=="1"):
+		ser.writelines(b'H')
+	elif(data=="0"):
+		ser.writelines(b'L')
+	time.sleep(0.1)
 
 ser.close()
+
